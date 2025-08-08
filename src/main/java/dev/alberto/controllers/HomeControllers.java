07714d51c9e1a298b970f90d9610f50b2e31e66f
Bertopf <bertoperez.apf@gmail.com>
2025-08-08 13:52:16 +0200

    IO-4-HomeControllers

---
+++ src/main/java/dev/alberto/controllers/HomeControllers.java
@@ -0,0 +1,14 @@
+package dev.alberto.controllers;
+import dev.alberto.views.HomeView;
+
+public class HomeControllers {
+
+    public HomeControllers() {
+        index();
+    }
+
+    public void index() {
+        HomeView.printMenu();
+    }
+    
+}
