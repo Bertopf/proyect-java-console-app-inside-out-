2ba8e999dcef6f6b346cb356ebf4090edba3a07e
Bertopf <bertoperez.apf@gmail.com>
2025-08-08 12:57:50 +0200

    IO-4-feat-created the menu on HomeView.java

--- src/main/java/dev/alberto/views/HomeView.java
+++ src/main/java/dev/alberto/views/HomeView.java
@@ -1,5 +1,18 @@
 package dev.alberto.views;
 
-public class HomeView {
+public class HomeView extends View {
+    public static void printMenu() {
+        String text = """
+                1. Añadir momento
+                2. Ver todos los momentos disponibles
+                3. Eliminar un momento
+                4. Filtrar los momentos
+                5. Salir
+                Seleccione una opción: """;
+        System.out.print(text);
+        int option = SCANNER.nextInt();
+    }
+
+    
     
 }
