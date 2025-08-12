8c057b1126227fb9c1fbc7f390ac8da2f84e5e67
Bertopf <bertoperez.apf@gmail.com>
2025-08-12 12:09:20 +0200

    fix-momentDTO

--- src/main/java/dev/alberto/dtos/MomentDTO.java
+++ src/main/java/dev/alberto/dtos/MomentDTO.java
@@ -3,9 +3,7 @@ package dev.alberto.dtos;
 import dev.alberto.models.EmotionEnum;
 
 public record MomentDTO(String title, String description, String date, EmotionEnum emotion) {
-    // This record will automatically generate the constructor, getters, and toString method
-    // for the fields title, description, date, and emotion.
-    // No additional code is needed here.
+    
     
     
 }
