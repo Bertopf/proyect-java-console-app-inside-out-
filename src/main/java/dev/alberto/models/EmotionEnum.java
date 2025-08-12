40de444f1af769a8aaa386baf7e6822be8d68e54
Bertopf <bertoperez.apf@gmail.com>
2025-08-12 11:38:11 +0200

    IO-6-feature-emotions list

---
+++ src/main/java/dev/alberto/models/EmotionEnum.java
@@ -0,0 +1,22 @@
+package dev.alberto.models;
+
+
+public enum EmotionEnum {
+    ALEGRIA("Alegría"),
+    TRISTEZA("Tristeza"),
+    IRA("Ira"),
+    ASCO("Asco"),
+    MIEDO("Miedo"),
+    ANSIEDAD("Ansiedad"),
+    ENVIDIA("Envidia"),
+    VERGUENZA("Vergüenza"),
+    ABURRIMIENTO("Aburrimiento"),
+    NOSTALGIA("Nostalgia");
+    private final String displayName;
+    EmotionEnum(String displayName) {
+        this.displayName = displayName;
+    }
+    public String getDisplayName() {
+        return displayName;
+    }
+}
--- src/main/java/dev/alberto/views/MomentPostView.java
+++ src/main/java/dev/alberto/views/MomentPostView.java
@@ -3,28 +3,31 @@ package dev.alberto.views;
 import dev.alberto.dtos.MomentDTO;
 import dev.alberto.controllers.MomentController;
 import dev.alberto.views.HomeView;
+import dev.alberto.models.EmotionEnum;
 
 
 public class MomentPostView extends View {
-
     private static MomentController CONTROLLER = HomeView.momentController;
-
     public static void printStoreMenu() {
-        System.out.println("Ingrese el titulo:");
+
+        System.out.println("Ingrese el título:");
         String title = SCANNER.next();
+
+        System.out.println("Ingrese la fecha: (dd/mm/aaaa)");
+        String date = SCANNER.next();
 
         System.out.println("Ingrese la descripción:");
         String description = SCANNER.next();
 
-        System.out.println("Ingrese la fecha (YYYY-MM-DD):");
-        String date = SCANNER.next();
-
-        MomentDTO momentDTO = new MomentDTO(title, description, date);
+        System.out.println("Seleccione la emoción:");
+        for (EmotionEnum e : EmotionEnum.values()) {
+            System.out.println((e.ordinal() + 1) + ". " + e.getDisplayName());
+        }
+        int emotionChoice = SCANNER.nextInt();
+        EmotionEnum emotion = EmotionEnum.values()[emotionChoice - 1];
+        
+        MomentDTO momentDTO = new MomentDTO(title, date, description, emotion);
         CONTROLLER.StoreMoment(momentDTO);
-
         HomeView.printMenu();
-
     }
-
-    
 }
