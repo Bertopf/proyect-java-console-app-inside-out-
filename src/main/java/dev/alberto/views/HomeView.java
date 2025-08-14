51ac10a0c92aadb22378dd2733908213724bcca7
Bertopf <bertoperez.apf@gmail.com>
2025-08-14 11:32:13 +0200

    IO-12-fix-menu options

--- src/main/java/dev/alberto/views/HomeView.java
+++ src/main/java/dev/alberto/views/HomeView.java
@@ -20,12 +20,11 @@ public class HomeView extends View {
         int option = SCANNER.nextInt();
         if (option == 1) {
                 MomentPostView.printStoreMenu();
-            }
+         }
 
         if (option == 2) {
             momentController.ShowAllMoments();
-            
-            
+            printMenu();
         }
 
         if (option == 3) {
@@ -35,6 +34,17 @@ public class HomeView extends View {
         if (option == 4) {
             MomentFilterView.printFilterMenu();
         }
+
+        if (option == 5) {
+            System.out.println("Saliendo...");
+            System.exit(0);
+        }
+        
+        if (option < 1 || option > 5) {
+            System.out.println("Opción inválida. Inténtalo de nuevo.");
+            printMenu();
+        }
+
     }
 
     
