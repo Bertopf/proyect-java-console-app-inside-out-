c22b8b429241eda67ea1f5baf8658518dcc615e3
Bertopf <bertoperez.apf@gmail.com>
2025-08-14 14:21:51 +0200

    IO-13-test momentcontrollersingleton

---
+++ src/test/java/dev/alberto/singletons/MomentControllerSingletonTest.java
@@ -0,0 +1,21 @@
+package dev.alberto.singletons;
+
+
+import dev.alberto.controllers.MomentController;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+class MomentControllerSingletonTest {
+    @Test
+    void getInstance_shouldReturnSameInstance() {
+        MomentController instance1 = MomentControllerSingleton.getInstance();
+        MomentController instance2 = MomentControllerSingleton.getInstance();
+        assertThat(instance1, is(notNullValue()));
+        assertThat(instance1, is(sameInstance(instance2)));
+    }
+    @Test
+    void getInstance_shouldReturnMomentControllerType() {
+        MomentController instance = MomentControllerSingleton.getInstance();
+        assertThat(instance, is(instanceOf(MomentController.class)));
+    }
+}
