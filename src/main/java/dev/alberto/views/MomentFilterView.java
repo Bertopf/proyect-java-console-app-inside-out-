0162983c9e53c16cc3e5137436e69f0ead5e2dbb
Bertopf <bertoperez.apf@gmail.com>
2025-08-19 12:37:21 +0200

    dev-fix- filter good bad in moment filter view

--- src/main/java/dev/alberto/views/MomentFilterView.java
+++ src/main/java/dev/alberto/views/MomentFilterView.java
@@ -5,6 +5,7 @@ public class MomentFilterView extends View {
         System.out.println("Filtrar por ...:");
         System.out.println("1. Emoción");
         System.out.println("2. Fecha");
+        System.out.println("3. Bueno o malo");
         int choice = SCANNER.nextInt();
         if (choice == 1) {
             MomentsFilterByEmotionView.printFilterMenu();
@@ -12,7 +13,7 @@ public class MomentFilterView extends View {
             MomentFilterByDateView.printFilterMenu();
 
         } else if (choice == 3) {
-            HomeView.printMenu();
+            MomentFilterByGoodBadEmotionView.printFilterMenu();
         } else {
             System.out.println("Opción inválida. Intentelo de nuevo.");
             printFilterMenu();
